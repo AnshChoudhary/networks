212c1dd40da74da91026296599a2d022fbc3dcb1
Ansh Choudhary <32743873+AnshChoudhary@users.noreply.github.com>
2024-05-16 16:39:21 +0400

    Update ClientHandler1.java

--- TCP/ClientHandler1.java
+++ TCP/ClientHandler1.java
@@ -12,13 +12,10 @@ public class ClientHandler1 implements Runnable
             PrintWriter out = null;
             BufferedReader in = null;
             try {
-                // get the outputstream of client
                 out = new PrintWriter(clientSocket.getOutputStream(), true);  
-                // get the inputstream of client
                 in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));  
                 String line;
                 while ((line = in.readLine()) != null) {
-                    // writing the received message from client
                     System.out.printf(" Sent from the client: %s\n",line);
                     out.println(line);
                     out.flush();
